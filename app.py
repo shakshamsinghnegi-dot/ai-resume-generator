c85d253fabd1c475a33eaa766b2a8171170c9dc2
shakshamsinghnegi-dot <shakshamsinghnegi@gmail.com>
2026-07-28 13:26:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,20 +113,21 @@ query = final_prompt+user_details
 
 import base64
 
-OPTION = ["DELHI","NOIDA","BANGLORE"]
-LOCATION = st.sidebar.multiselect('SELECT LOCATION: ',
-                                 options = vOPTIONS)
-JOB_PROFILE = ["PYTHON DEVELOPER","'GEN AI',
-               'FULL-STACK DEVELOPER','DATA ANALYST']
 
+OPTIONS = [("DELHI", "NOIDA", "GURGAON/GURUGRAM",
+'KANPUR', 'LUCKNOW', 'BANGLORE', 'PUNE]
+LOCATION = st.sidebar.multiselect('SELECT LOCATION:',
+options = OPTIONS)
+JOB_PROFILE = ["PYTHON DEVELOPER", 'GEN AI',
+'FULL-STACK DEVELOPER', 'DATA ANALYST']
 PROFILE = st.sidebar.multiselect("SELECT JOB ROLE",
-                                 options = JOB_PROFILE)
-
-job_prompt = f"""Based on {PROFILE} jobs in {LOCATION}, 
-I want latest job news in using tavily,
-try top 10 search or whatever available and give result
-like naukri theme design with job name, job desc, salary, 
-apply link""
+options = JOB_PROFILE)
+job_prompt = f"""Based on (PROFILE) jobs in {LOCATION}, I
+want latest job news in using tavily,
+try top 10 search or whatever available
+and give result like naukri theme design with
+job name, job desc, salary,
+apply linkand  output must be in html""
 
 
 if st.button('generate resume'):
